5a87e75de7a8e83cd27712304da8af1dcc8e63cb
Rel-ve <116023951+Rel-ve@users.noreply.github.com>
2022-10-17 22:55:35 +0530

    Create rps.py
    A simple Rock, Paper Scissors program

---
+++ rps.py
@@ -0,0 +1,31 @@
+import random
+
+def game(comp,a): # Makes a function that compares the value provided by the computer and the player  
+    if a == comp:
+      print("Draw")
+    elif a != comp:
+        if ((comp == 's' and a == 'r') or (comp == 'p' and a == 's')):
+            print("Player wins!") 
+        elif ((comp == 'r' and a == 's') or (comp == 's' and a == 'p')):
+            print("Computer wins!") 
+        elif comp == 'r' and a == 'p':
+            print("Player wins!")
+        elif comp == 'p' and a == 'r':
+            print("Computer wins")
+
+num = random.randint(1,3) # Picks a random number
+
+if num == 1: # Converts num to r, p, s
+    comp = 's'
+elif num == 2:
+    comp = 'r'
+else:
+    comp = 'p'
+
+choice = input("Enter Scissors(s), Rock(r), Paper(p): ") 
+
+if choice == 's' or choice == 'r' or choice == 'p': # Compares the values inputed by the user
+   print("Choice of computer", comp)
+   game(choice,comp)
+else:
+    print("Input valid statement")
